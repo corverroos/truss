f9b1d044dd068495c1a7fe23fa0cee77f5c9a014
corver <corver@luno.com>
2020-09-14 14:54:53 +0200

    truss: Improve readme and generated by to schema

--- db_test.go
+++ db_test.go
@@ -1,10 +1,15 @@
 package truss_test
 
 import (
+	"flag"
 	"testing"
 
 	"github.com/corverroos/truss"
 )
+
+var update = flag.Bool("update", false, "update schema file")
+
+//go:generate go test -update -run=TestTestSchema
 
 func TestTestSchema(t *testing.T) {
 	ql := []string{
@@ -13,6 +18,9 @@ func TestTestSchema(t *testing.T) {
 		"CREATE TABLE test3 (id TINYINT, name CHAR(3), PRIMARY KEY(id));",
 	}
 
+	if !*update {
+		truss.TestSchema(t, "testdata/schema.sql", false, ql...)
+	}
 	truss.TestSchema(t, "testdata/schema.sql", true, ql...)
 	truss.TestSchema(t, "testdata/schema.sql", false, ql...)
 }
